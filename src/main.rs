2c98138a73dc20f262c918875396301998075117
Alex Helfet <alex.helfet@gmail.com>
2023-03-07 21:55:19 +0000

    Added examples about name resolution, use, and pub use.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+// Mute some warnings by unused code in the examples.
+#![allow(dead_code, unused_imports)]
+
 // # Rust modules example
 
 // Most of the time a module means a file, but there are other
@@ -95,7 +98,163 @@ fn use_platform() -> &'static str {
 // Dependency crates can also be compiled conditionally based on the
 // build target, enabled feature flags, and other factors.
 
+// ## Name resolution
+mod name_resolution {
+    // Everything is private by default in Rust, including modules.
+    // `private_inner` is visible in `name_resolution` and its child
+    // modules, but not by `name_resolution`'s parents.
+    mod private_inner {
+
+        // `a` is visible to `private_inner` and its child modules, but nowhere else.
+        fn a() {}
+
+        // `pub` keyword makes an item exported by its parent module,
+        // so `b` is visible to any module that can see
+        // `private_inner`, but `main` cannot see `private_inner`, so it still
+        // cannot see `b`.
+        pub fn b() {}
+    }
+
+    fn test_private_inner() {
+        // Access a child module's exported item with the syntax `${child_module}::{item}`:
+        private_inner::b();
+
+        // This will not compile, because `a` is not exported with `pub`:
+        // private_inner::a();
+    }
+
+    // `main` sees this module and everything exported by it.
+    pub mod public_inner {
+        pub fn a() {}
+    }
+}
+
+// ## Imports with `use`
+
+// Items visible in a scope can be imported into that scope with the `use` keyword:
+mod use_examples {
+
+    mod use_inner {
+        pub fn a() {}
+        pub fn b() {}
+    }
+
+    // `use_inner::a` is now in scope as `a` in `use_examples`.
+    use use_inner::a;
+
+    fn test_use() {
+        // `use_inner::a` can be called explicitly.
+        use_inner::a();
+
+        // But because we imported it with `use` we can also call it simply as `a`.
+        a();
+
+        // Function definitions also have a scope, so can contain `use` statements,
+        // which work as you'd expect.
+        use use_inner::b;
+        b();
+    }
+
+    mod use_wildcard {
+        pub fn not() {}
+        pub fn my() {}
+        pub fn favourite() {}
+    }
+
+    // `use` also supports wildcards.
+    // I personally don't like this because with multiple wildcards it's more difficult
+    // to know where a particular item comes from.
+    use use_wildcard::*;
+
+    fn test_use_wildcard() {
+        not();
+        my();
+        favourite();
+    }
+
+    // `use` supports renaming, useful to avoid name clashes:
+
+    mod use_rename {
+        pub fn a() {}
+    }
+
+    // This would be a compile error, because we already imported `use_inner::a` above.
+    // use use_rename::a;
+
+    // This imports `use_rename::a` as `a_renamed`.
+    use use_rename::a as a_renamed;
+
+    // `use` supports a nested syntax which avoids repetition in imports:
+
+    mod use_nested_1 {
+        pub mod use_nested_2 {
+            pub fn g() {}
+            pub fn h() {}
+        }
+
+        pub mod use_nested_3 {
+            pub fn g() {}
+            pub fn i() {}
+        }
+
+        pub fn j() {}
+    }
+
+    use use_nested_1::{
+        use_nested_2::{g, h},
+        use_nested_3::{g as use_nested_3_g, i},
+        j
+    };
+
+    fn test_use_nested() {
+        g();
+        use_nested_3_g();
+        h();
+        i();
+        j();
+    }
+
+    // ### Advanced: re-exporting.
+
+    // Items visible in a scope can be exported by that scope with the `pub use` keywords:
+    mod inner_1 {
+        mod inner_2 {
+            pub fn x() {}
+        }
+
+        // `inner_2::x` is now imported into `inner_1`'s scope as `x`, but it has also been
+        // exported because of the `pub`.
+        pub use inner_2::x;
+    }
+
+    fn test_pub_use() {
+        // This doesn't compile, because `inner_2` is not exported by `inner_1`.
+        // inner_1::inner_2::x();
+
+        // This works fine and refers to the same function, because
+        // `inner_1` exported `inner_2::x` with `pub use`.
+        inner_1::x();
+    }
+
+    // ----
+
+    // `use` and `pub use` can be applied to almost any item in a module, including:
+    //
+    // * `const`
+    // * `enum`
+    // * `fn`
+    // * `mod`
+    // * `static`
+    // * `struct`
+    // * `trait`
+    // * `type`
+
+    // Macros have some different rules, I may come back to explain those another time.
+}
+
 fn main() {
     println!("Hello, world! Running on platform family '{}'", use_platform());
     inline::inline_fn();
+
+    name_resolution::public_inner::a();
 }
